68f39d4cc5b85c849b27880b4d5760950f24fddc
James O'Neal <joneal@ppi2pass.com>
2016-10-23 08:28:22 -0700

    remove blank license headers

--- app/code/local/Jamesooo/EmailTracking/Model/Emailtracking.php
+++ app/code/local/Jamesooo/EmailTracking/Model/Emailtracking.php
@@ -1,10 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 class Jamesooo_EmailTracking_Model_Emailtracking extends Mage_Reports_Model_Mysql4_Order_Collection{
     function __construct() {
         parent::__construct();
--- app/code/local/Jamesooo/EmailTracking/Model/Resource/Mysql4/Setup.php
+++ app/code/local/Jamesooo/EmailTracking/Model/Resource/Mysql4/Setup.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joneal
- * Date: 8/13/2015
- * Time: 12:51 PM
- */
-
 class Jamesooo_EmailTracking_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup{
 
 }
